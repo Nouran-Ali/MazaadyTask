6bc696b38f285d495f6b27ae7130185ba93a5fcf
Nouran-Ali <noranali237@gmail.com>
2024-03-02 20:10:58 +0200

    finish the form

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import Layout from "./../comps/Layout";
+import { Head } from "next/document";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </>;
 }
